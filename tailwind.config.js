00103ddf0330e7ed5a34ae3b9e3ca58780ca5026
WPondian <willian.pondian@outlook.com>
2024-05-17 17:07:54 -0300

    adding class customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGreen: {
+          1: "#484848",
+          2: "#006465",
+          3: "#0f928c",
+          4: "#00c9d2",
+          5: "#beee3b",
+        },
+        customRed: {
+          1: "#b7aea5",
+          2: "#433d3d",
+          3: "#221d21",
+          4: "#f77014",
+          5: "#e33c08",
+        },
+        customBlue: {
+          1: "#343838",
+          2: "#005f6b",
+          3: "#008c9e",
+          4: "#00b4cc",
+          5: "#00dffc",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
